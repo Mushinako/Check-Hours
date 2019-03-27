10ea2cc92eb58b1444f10b78311ab14015efd3c4
Mushinako <ridoedee@gmail.com>
2019-03-27 04:10:53 -0700

    Repo FAQ

---
+++ js/faq.js
@@ -0,0 +1,9 @@
+let byId = (e) => document.getElementById(e);
+
+window.onload = () => {
+  byId('home').href = `http://${ip}`;
+  byId('report').href = `http://${ip}/report.html`;
+
+  byId('cheat').href = '#';
+  $('#cheat').on('click', () => window.location.href = 'https://youtu.be/dQw4w9WgXcQ?t=42');
+}
