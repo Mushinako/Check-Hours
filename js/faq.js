abb591ad205e15aa4e5cfdfb818691b93eee9850
Mushinako <ridoedee@gmail.com>
2019-04-01 14:27:56 -0700

    Link Anchors

--- js/faq.js
+++ js/faq.js
@@ -5,5 +5,5 @@ window.onload = () => {
   byId('report').href = `http://${ip}/report.html`;
 
   byId('cheat').href = '#';
-  $('#cheat').on('click', () => window.location.href = 'https://youtu.be/dQw4w9WgXcQ?t=42');
+  $('#cheat').on('click', () => window.location.href = 'https://youtu.be/dQw4w9WgXcQ?t=43');
 }
