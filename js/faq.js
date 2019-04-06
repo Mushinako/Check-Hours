89f3f8946f5c27d1dda494ad6793fd1dc2a52d1b
Mushinako <ridoedee@gmail.com>
2019-04-06 15:08:50 -0700

    Hmm

--- js/faq.js
+++ js/faq.js
@@ -1,5 +1,4 @@
 let byId = (e) => document.getElementById(e);
-let lastPos = 0;
 
 window.onload = () => {
   // byId('home').href = `http://${ip}`;
@@ -7,10 +6,6 @@ window.onload = () => {
 
   byId('cheat').href = '#';
   $('#cheat').on('click', () => window.location.href = 'https://youtu.be/dQw4w9WgXcQ?t=43');
-}
 
-$(document).bind('hashchange', () => {
-  lastPos = $(window).scrollTop();
-  location.hash = '';
-  $(window).scrollTop(lastPos);
-});
+  $('.hash').on('click', (e) => byId(e.currentTarget.id + 'a').scrollIntoView({block: 'nearest'}));
+}
