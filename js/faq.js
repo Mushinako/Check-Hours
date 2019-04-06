c3d5ba368bc59d942846215ed80720be01328e24
Mushinako <ridoedee@gmail.com>
2019-04-06 14:45:58 -0700

    Update faq.js

--- js/faq.js
+++ js/faq.js
@@ -1,4 +1,5 @@
 let byId = (e) => document.getElementById(e);
+let lastPos = 0;
 
 window.onload = () => {
   // byId('home').href = `http://${ip}`;
@@ -7,3 +8,9 @@ window.onload = () => {
   byId('cheat').href = '#';
   $('#cheat').on('click', () => window.location.href = 'https://youtu.be/dQw4w9WgXcQ?t=43');
 }
+
+$(document).bind('hashchange', () => {
+  lastPos = $(window).scrollTop();
+  location.hash = '';
+  $(window).scrollTop(lastPos);
+});
