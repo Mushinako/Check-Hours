3b06463823ffa99a8a35df555659d569a196b17c
Mushinako <ridoedee@gmail.com>
2019-08-30 17:06:35 -0700

    Update faq.js
    * Open in New Tab

--- js/faq.js
+++ js/faq.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   byId('report').href = `http://${ip}/report.html`;
 
   byId('cheat').href = '#';
-  byId('cheat').onclick = () => window.location.href = 'https://youtu.be/dQw4w9WgXcQ?t=43';
+  byId('cheat').onclick = () => window.open('https://youtu.be/dQw4w9WgXcQ?t=43');
 
   for (let a of document.getElementsByClassName('hash')) a.onclick = (e) => byId(a.id+'a').scrollIntoView({block:'nearest'});
 });
