ec0f8d9e53b55d67328c0fee7cc1bdb24273728f
Mushinako <ridoedee@gmail.com>
2019-08-30 17:20:38 -0700

    Clunky!

--- js/faq.js
+++ js/faq.js
@@ -3,7 +3,7 @@ let byId = (e) => document.getElementById(e);
 document.addEventListener('DOMContentLoaded', () => {
   byId('report').href = `http://${ip}/report.html`;
 
-  byId('cheat').href = '#';
+  byId('cheat').removeAttribute('href');
   byId('cheat').onclick = () => window.open('https://youtu.be/dQw4w9WgXcQ?t=43');
 
   for (let a of document.getElementsByClassName('hash')) a.onclick = (e) => byId(a.id+'a').scrollIntoView({block:'nearest'});
