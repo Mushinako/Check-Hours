a1e944badaa8060670e7122674ad29e4acb83472
Mushinako <ridoedee@gmail.com>
2019-09-07 17:10:14 -0700

    White theme hotfix

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // Bug Reports Welcomed!
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('noscript').style.display = 'none';
+  document.getElementById('script').style.display = 'block';
   window.location.replace(`http://${ip}/index.html`);
 });
